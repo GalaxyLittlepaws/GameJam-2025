ccd6fb627d27140a992c888e085e58f6f55844a2
GalaxyLittlepaws <galaxylittlepaws@proton.me>
2025-04-03 13:11:56 -0400

    ✨ Fleshes out Inventory a bit more, adds a starter scene with rough content layout

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -3,14 +3,29 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour {
 
-    [SerializeField] List<InventoryObject> inventoryObjects;
-    // -1 means no object is currently selected
-    [SerializeField] int currentlySelected = -1;
+    [SerializeField] List<InventoryObject> inventoryObjects = new List<InventoryObject>();
+    [SerializeField] private int currentlySelected = -1;
+
     void Start() {
         
     }
 
     void Update() {
-        
+        // Handle inventory updates if needed
+    }
+
+    public void AddItem(InventoryObject item) {
+        inventoryObjects.Add(item);
+    }
+
+    public void RemoveItem(InventoryObject item) {
+        inventoryObjects.Remove(item);
+    }
+
+    public InventoryObject GetSelectedItem() {
+        if (currentlySelected >= 0 && currentlySelected < inventoryObjects.Count) {
+            return inventoryObjects[currentlySelected];
+        }
+        return null;
     }
 }
