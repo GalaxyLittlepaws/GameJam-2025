6267366baf68f1f4687b5a0b3e6662cd42d36890
Ryan Zollinger <ryanzolly@gmail.com>
2025-04-12 17:08:50 -0600

    settings workign now

--- Assets/Scripts/SettingsScripts/ColourButton.cs
+++ Assets/Scripts/SettingsScripts/ColourButton.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 public class ColourButton : MonoBehaviour {
     [SerializeField] TextMeshProUGUI tMPro;
     AccessibilityManager accessibilityManager;
+    [SerializeField] TextMeshProUGUI colourVal;
     void Start() {
         accessibilityManager = FindAnyObjectByType<AccessibilityManager>();
     }
 
+    public void Cycle() {
+        accessibilityManager.currentColour++;
+        if (accessibilityManager.currentColour == accessibilityManager.colourList.Count) {
+            accessibilityManager.currentColour = 0;
+        }
+    }
     void Update() {
-        
+        if(accessibilityManager.currentColour == 0) {
+            colourVal.text = "Default";
+        } else if(accessibilityManager.currentColour == 1) {
+            colourVal.text = "White";
+        } else {
+            colourVal.text = accessibilityManager.colourList[accessibilityManager.currentColour].ToString();
+        }
     }
 }
